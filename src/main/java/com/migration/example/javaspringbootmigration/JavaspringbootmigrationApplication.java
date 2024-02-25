5d4f06b14c07aade82d81d296cd106e8bc1417cb
Viswanathan Ramachandran <viswanathan057@gmail.com>
2024-02-25 15:51:38 -0500

    first commit

---
+++ src/main/java/com/migration/example/javaspringbootmigration/JavaspringbootmigrationApplication.java
@@ -0,0 +1,14 @@
+package com.migration.example.javaspringbootmigration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class JavaspringbootmigrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaspringbootmigrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/migration/example/javaspringbootmigration/JavaspringbootmigrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.migration.example.javaspringbootmigration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaspringbootmigrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
